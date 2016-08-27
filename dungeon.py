8baaebf8e13444ad8e77541950eb87d05119f1d8
benjbubu <benjbubu@barakuun.com>
2016-08-27 20:35:51 +0200

    Modification du jet de MJ et ajout de commentaire

--- dungeon.py
+++ dungeon.py
@@ -1,13 +1,16 @@
 import random
 import math
 
+#Declaration des items trouvables dans les de et des loot pour les random
 playerdice = ['Guerrier', 'Voleur', 'Mage', 'Clerc', 'Champion', 'Parchemin']
 mjdice = ['Gobelin', 'Blob', 'Squelette', 'Coffre', 'Dragon', 'Potion']
 tresor = ['Portail de ville', 'Appat de Dragon', 'Anneau dinvisibilite', 'Potion', 'Parchemin', 'Epee Vorpale', 'Talisman', 'Sceptre de Pouvoir', 'Outil de Voleur']
 
-niveaudonjon = 1
+#Declaration du niveau du hero et du donjon au debut du jeu
+niveaudonjon = 4
 niveauhero = 1
 
+#Lancement des des du joueur
 print("Bienvenue dans le Donjon. Vous etes niveau", niveauhero , ". Voyons qui vous accompagne")
 d1 = random.choice(playerdice)
 d2 = random.choice(playerdice)
@@ -20,6 +23,14 @@ d7 = random.choice(playerdice)
 playerhand = [d1, d2, d3, d4, d5, d6, d7]
 print(playerhand)
 
+
 print("Vous arrivez au niveau", niveaudonjon, ". Des monstres apparaissent !")
+#Lancement de des du MJ
+mjhand = []
 for i in range(niveaudonjon):
-	print(random.choice(mjdice))
+	mjhand.append(random.choice(mjdice))
+
+print(mjhand)	
+print("Que souhaitez-vous faire ?")
+
+
