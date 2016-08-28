9b968eab48f6e568dc5acbb00aee42a599452610
Louc <luc@lucmasson.info>
2016-08-28 21:45:00 +0200

    ajout phase regroupement. A CORRIGER : phase baston, usage des parchemins

--- dungeon.py
+++ dungeon.py
@@ -19,29 +19,30 @@ class DungeonRollGame:
         self.antre_dragon = []
 
         # Declaration du niveau du hero et du donjon au debut du jeu
-        self.niveau_donjon = 4
+        self.niveau_donjon = 9
         self.niveau_hero = 1
 
         # Lancement des dés du joueur
         self.afficher("Bienvenue dans le Donjon. Vous etes niveau " + str(self.niveau_hero) + ". Voyons qui vous accompagne")
         self.player_hand = []
+
         for i in range(1, 7):
-            self.player_hand.append(random.choice(self.compagnons.keys()))
+            self.player_hand.append(random.choice(list(self.compagnons.keys())))
         self.afficher(self.player_hand)
 
         # Tour de jeu
         self.continuer_partie = True
         while self.continuer_partie:
             self.entrer_dans_donjon()
-            self.bastonner()
-            self.piller_butin()
-            self.reflechir_avenir()
+            if self.bastonner() == "Succes":
+                self.piller_butin()
+                self.reflechir_avenir()
             
         # Fin de la partie => calcul score 
         self.afficher("C'est la FIN")
 
     def entrer_dans_donjon(self):
-        self.afficher("Vous arrivez au niveau" + str(self.niveau_donjon) + ". Des monstres apparaissent !")
+        self.afficher("Vous arrivez au niveau " + str(self.niveau_donjon) + ". Des monstres apparaissent !")
 
         # Lancement de des du MJ
         self.monstres = []
@@ -58,6 +59,7 @@ class DungeonRollGame:
         self.afficher(str(self.antre_dragon) + str(self.loot) + str(self.monstres))    
 
     def bastonner(self):
+        exploration = "Succes"
         while self.monstres:
             # Choisir une action 
             possibilites = ["utiliser un TRésor", "utiliser un COmpagnon", "FUir"]
@@ -69,18 +71,33 @@ class DungeonRollGame:
             # Résolution de l'action
             if choix in ("FU", "Fuir", "FUir", "fuir", "F", "f"):
                 self.continuer_partie = False
+                exploration = "Echec"
                 break
             elif choix in ("TR", "TRésor", "Trésor", "trésor", "TResor", "Tresor", "tresor", "T", "t"):
                 self.afficher("Fonction non implémentée")
             elif choix in ("COmpagnon", "Compagnon", "compagnon", "C", "c"):
                 self.utiliser_compagnon()
 
+        return exploration  # "Echec" ou "Succes"
+
     def piller_butin(self):
         pass
 
     def reflechir_avenir(self):
-        # pour l'instant on continue toujours
-        self.niveau_donjon += 1
+        # donjon max = niv 10 => fin de la partie
+        if self.niveau_donjon >= 10:
+            self.continuer_partie = False
+            self.afficher("Niveau 10 atteint, c'est la fin de la partie !")
+            return
+
+        # choisir entre continuer et partir
+        choix = self.recuperer("Vous pouvez COntinuer l'aventure ou vous REtirer...")
+        if choix in ("COntinuer", "Continuer", "Continuer", "C", "c"):
+            # La partie continue
+            self.niveau_donjon += 1
+        else:
+            self.continuer_partie = False
+
 
     def utiliser_compagnon(self):
         # choix du compagnon
